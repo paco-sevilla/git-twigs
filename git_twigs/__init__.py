4b5674ce32176eeadc5ed65c9fdee5ffb63e3801
Paco Sevilla <paco.sevilla@live.com>
2024-04-02 00:40:06 +0200

    Add copyright statements and fix `mypy` errors

--- git_twigs/__init__.py
+++ git_twigs/__init__.py
@@ -1,3 +1,9 @@
+# Copyright 2024: The `git-twigs` authors
+#
+# This file is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+# License, version 2 (GNU GPLv2).This software is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY. See the `LICENSE` file for more details.
+#
 """A user-friendly Git extension to track branch creation, rebasing and stacking."""
 
 __version__ = "0.0.1"
--- git_twigs/cli.py
+++ git_twigs/cli.py
@@ -1,8 +1,14 @@
+# Copyright 2024: The `git-twigs` authors
+#
+# This file is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+# License, version 2 (GNU GPLv2).This software is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY. See the `LICENSE` file for more details.
+#
 """CLI entry points for git-twigs."""
 
 import click
 
 
 @click.group()
-def main():
+def main() -> None:
     """git-twig - List, create, or delete twigs."""
